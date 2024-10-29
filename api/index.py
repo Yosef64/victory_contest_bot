e11c8310616ad44f4c2e93eabeb665e84f949ee6
Yosef64 <yosefale65@gmail.com>
2024-10-30 01:33:00 +0300

    fixing

--- api/index.py
+++ api/index.py
@@ -6,27 +6,28 @@ app = FastAPI()
 
 TOKEN = '7897490261:AAFMKWSSK0wHuSHlROpQH5WW9v4VsSTlkoA'
 WEBHOOK_URL = 'https://jositgwebapp.vercel.app/api/index'
-bot = Bot(token=TOKEN)
 
-# Initialize the Telegram Bot application globally
-application = ApplicationBuilder().token(TOKEN).build()
+# Define bot and the handler functions
+bot = Bot(token=TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [[InlineKeyboardButton("Visit Web App", web_app={"url": "https://josialex.vercel.app/"})]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Welcome! Click the button below to visit our web app.", reply_markup=reply_markup)
 
-def register_handlers():
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-
-register_handlers()
+# Function to initialize Application and register handlers
+def initialize_application():
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler('start', start))
+    return application
 
 @app.post("/webhook")
 async def webhook(request: Request, background_tasks: BackgroundTasks):
     """
     Telegram Webhook
     """
+    application = initialize_application()
+
     request_data = await request.json()
     update = Update.de_json(request_data, bot)
 
