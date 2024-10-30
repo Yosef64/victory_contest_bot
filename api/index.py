c6b491d78607f7b88979d6ff3047a32841f5acba
Yosef64 <yosefale65@gmail.com>
2024-10-30 11:56:55 +0300

    changing ptb on post

--- api/index.py
+++ api/index.py
@@ -5,35 +5,30 @@ from telegram.ext import Application, CommandHandler
 from telegram.ext._contexttypes import ContextTypes
 from fastapi import FastAPI, Request, Response
 
-ptb = (
+
+
+app = FastAPI()
+async def start(update, _: ContextTypes.DEFAULT_TYPE):
+    """Send a message when the command /start is issued."""
+    await update.message.reply_text("starting...") 
+@app.post("/")
+async def process_update(request: Request):
+    ptb = (
     Application.builder()
     .updater(None)
     .token("7897490261:AAFMKWSSK0wHuSHlROpQH5WW9v4VsSTlkoA") 
     .read_timeout(7)
     .get_updates_read_timeout(42)
     .build()
-)
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    await ptb.bot.setWebhook() 
-    async with ptb:
-        await ptb.start()
-        yield
-        await ptb.stop()
-
-app = FastAPI(lifespan=lifespan)
-@app.post("/")
-async def process_update(request: Request):
+   )
+    ptb.add_handler(CommandHandler("start", start))
     req = await request.json()
+    
     update = Update.de_json(req, ptb.bot)
     await ptb.process_update(update)
     return Response(status_code=HTTPStatus.OK)
 @app.get("/")
 def index(request:Request):
     return {"message": "Hello World"}
-async def start(update, _: ContextTypes.DEFAULT_TYPE):
-    """Send a message when the command /start is issued."""
-    await update.message.reply_text("starting...") 
 
-ptb.add_handler(CommandHandler("start", start))
+
