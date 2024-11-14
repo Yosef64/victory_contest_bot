4cb847eaec036736a49870c09f12829c4b04fffa
Yosef64 <yosefale65@gmail.com>
2024-11-14 10:43:31 +0300

    changingg web app url

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 async def start(update, _: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("Visit Web App", web_app={"url": "https://josialex.vercel.app/"})]]
+    keyboard = [[InlineKeyboardButton("Visit Web App", web_app={"url": "https://victory-contest.vercel.app/"})]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Welcome! Click the button below to visit our web app.", reply_markup=reply_markup)
 @app.post("/")
