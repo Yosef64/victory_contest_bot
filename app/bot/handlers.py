a1799c4104bb379dd8b3fd9355921a7add05971b
Yosef64 <yosefale65@gmail.com>
2025-03-15 02:57:28 +0300

    fixing bugs

--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -1,7 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext import CommandHandler, CallbackContext,ContextTypes
 
-async def start(update:Update,context:CallbackContext):
+async def start(update,_:ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("Visit Web App", web_app={"url": "https://victory-contest.vercel.app/"})]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Welcome! Click the button below to visit our web app.", reply_markup=reply_markup)
