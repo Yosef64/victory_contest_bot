af850a32a2efafc5907fdb30049f7c27bd03762a
Yosef64 <yosefale65@gmail.com>
2025-03-15 10:47:41 +0300

    resolving the student by id

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,6 +2,6 @@ import requests
 from app.config.setting import BASE_API_URL
 
 async def check_user_register(tele_id):
-    res = await requests.get(f"{BASE_API_URL}/student/{tele_id}")
+    res = await requests.get(f"{BASE_API_URL}/api/student/{tele_id}")
     return res["student"]
 
