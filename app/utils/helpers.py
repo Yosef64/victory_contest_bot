6d75d77bc069e1e16677f244c6956559c4ac62d1
Yosef64 <yosefale65@gmail.com>
2025-03-15 10:59:37 +0300

    removing await fromo requst

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,8 +1,11 @@
+import httpx
 import requests
 from app.config.setting import BASE_API_URL
 
 async def check_user_register(tele_id):
-    res = requests.get(f"{BASE_API_URL}/api/student/{tele_id}")
-    print("result of the aoi call",res)
-    return res["student"]
+    async with httpx.AsyncClient() as client:
+        res = await client.get(f"{BASE_API_URL}/api/student/{tele_id}")
+        res.raise_for_status() 
+        data = res.json()
+        return data.get("student")
 
