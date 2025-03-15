7e4ceeb267edf48456783322910b175bdcf3db25
Yosef64 <yosefale65@gmail.com>
2025-03-15 10:49:10 +0300

    logging

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -3,5 +3,6 @@ from app.config.setting import BASE_API_URL
 
 async def check_user_register(tele_id):
     res = await requests.get(f"{BASE_API_URL}/api/student/{tele_id}")
+    print("result of the aoi call",res)
     return res["student"]
 
