4a644c509f75d4726e9f08e8e630ad7f249aeb11
Yosef64 <yosefale65@gmail.com>
2025-03-20 10:49:37 +0300

    fixing rules and regulaions

--- app/utils/variables.py
+++ app/utils/variables.py
@@ -1,32 +1,32 @@
 
 welcom_text = """
-🎉 Hey there\\, Champion\\! Welcome to Victory Contest\\! 🎉
+🎉 *Hey there\\, Champion\\! Welcome to Victory Contest\\!* 🎉
 
 We’re thrilled to have you here\\! 🚀 This is not just any contest—this is your chance to show off your skills\\, challenge yourself\\, and rise to the top\\! 🏆
 
-👀 What’s waiting for you?
+👀 *What’s waiting for you?*
 ✅ Exciting challenges that test your knowledge \\& speed
 ✅ A chance to compete with brilliant minds like you
 ✅ Leaderboards\\, rewards\\, and all the bragging rights\\!
 
-💡 But first… You need to register\\!
+💡 *But first… You need to register\\!*
 Just tap the button below\\, and you’ll be all set to jump into the action\\.
 
 👇 Hit the button \\& let the games begin\\! 👇
 
-We can’t wait to see you in action\\! 🎯🔥
+*We can’t wait to see you in action\\! 🎯*
 """
 welcome_back_txt = """
-🔥 Welcome back\\, Champion\\! 🔥
+🔥 *Welcome back\\, Champion\\!* 🔥
 
 Great to see you again\\! 🎉 We know you’re here to win\\, learn\\, and level up\\, and we’re ready to challenge you\\!
 
-🚀 What’s next?
+🚀 *What’s next?*
 ✅ Jump back into the contest and keep climbing the leaderboard 
 ✅ Test your skills with new challenges 
 ✅ Stay ahead of the competition \\& claim your spot at the top\\!
 
-Are you ready? Let’s get back in the game\\! 🎯💥
+Are you ready? Let’s get back in the game\\! 🎯
 """
 error_message = (
     """⚠️ *Oops\\, something went wrong\\!* 😢\n\n
@@ -38,7 +38,7 @@ error_message = (
 rules = (
     f"""📜 *Contest Rules \\& Guidelines* ⚖️\n\n
     🚀 *Ready to compete? Make sure you follow these rules\\!*\n\n
-    1️⃣ *One Account\\, One Contest\\!* ❌🆕\n"
+    1️⃣ *One Account\\, One Contest\\!* ❌🆕\n
        \\- You *CANNOT* enter the contest using *multiple accounts* at the same time\\.\n\n
     2️⃣ *No Late Entries\\!* ⏳🚫\n
        \\- Once the contest *ends*\\, you *cannot access it* anymore\\.\n\n
